7a5211e8cd9ce25f5d29ad89fd3f5de0284ce859
MohitS719 <59114009+MohitS719@users.noreply.github.com>
2019-12-25 18:32:47 +0530

    Add files via upload

---
+++ tg_bot/modules/blacklist.py
@@ -0,0 +1,175 @@
+import html
+import re
+from typing import Optional, List
+
+from telegram import Message, Chat, Update, Bot, ParseMode
+from telegram.error import BadRequest
+from telegram.ext import CommandHandler, MessageHandler, Filters, run_async
+
+import tg_bot.modules.sql.blacklist_sql as sql
+from tg_bot import dispatcher, LOGGER
+from tg_bot.modules.disable import DisableAbleCommandHandler
+from tg_bot.modules.helper_funcs.chat_status import user_admin, user_not_admin
+from tg_bot.modules.helper_funcs.extraction import extract_text
+from tg_bot.modules.helper_funcs.misc import split_message
+
+BLACKLIST_GROUP = 11
+
+BASE_BLACKLIST_STRING = "Current <b>blacklisted</b> words:\n"
+
+
+@run_async
+def blacklist(bot: Bot, update: Update, args: List[str]):
+    msg = update.effective_message  # type: Optional[Message]
+    chat = update.effective_chat  # type: Optional[Chat]
+
+    all_blacklisted = sql.get_chat_blacklist(chat.id)
+
+    filter_list = BASE_BLACKLIST_STRING
+
+    if len(args) > 0 and args[0].lower() == 'copy':
+        for trigger in all_blacklisted:
+            filter_list += "<code>{}</code>\n".format(html.escape(trigger))
+    else:
+        for trigger in all_blacklisted:
+            filter_list += " - <code>{}</code>\n".format(html.escape(trigger))
+
+    split_text = split_message(filter_list)
+    for text in split_text:
+        if text == BASE_BLACKLIST_STRING:
+            msg.reply_text("There are no blacklisted messages here!")
+            return
+        msg.reply_text(text, parse_mode=ParseMode.HTML)
+
+
+@run_async
+@user_admin
+def add_blacklist(bot: Bot, update: Update):
+    msg = update.effective_message  # type: Optional[Message]
+    chat = update.effective_chat  # type: Optional[Chat]
+    words = msg.text.split(None, 1)
+    if len(words) > 1:
+        text = words[1]
+        to_blacklist = list(set(trigger.strip() for trigger in text.split("\n") if trigger.strip()))
+        for trigger in to_blacklist:
+            sql.add_to_blacklist(chat.id, trigger.lower())
+
+        if len(to_blacklist) == 1:
+            msg.reply_text("Added <code>{}</code> to the blacklist!".format(html.escape(to_blacklist[0])),
+                           parse_mode=ParseMode.HTML)
+
+        else:
+            msg.reply_text(
+                "Added <code>{}</code> triggers to the blacklist.".format(len(to_blacklist)), parse_mode=ParseMode.HTML)
+
+    else:
+        msg.reply_text("Tell me which words you would like to remove from the blacklist.")
+
+
+@run_async
+@user_admin
+def unblacklist(bot: Bot, update: Update):
+    msg = update.effective_message  # type: Optional[Message]
+    chat = update.effective_chat  # type: Optional[Chat]
+    words = msg.text.split(None, 1)
+    if len(words) > 1:
+        text = words[1]
+        to_unblacklist = list(set(trigger.strip() for trigger in text.split("\n") if trigger.strip()))
+        successful = 0
+        for trigger in to_unblacklist:
+            success = sql.rm_from_blacklist(chat.id, trigger.lower())
+            if success:
+                successful += 1
+
+        if len(to_unblacklist) == 1:
+            if successful:
+                msg.reply_text("Removed <code>{}</code> from the blacklist!".format(html.escape(to_unblacklist[0])),
+                               parse_mode=ParseMode.HTML)
+            else:
+                msg.reply_text("This isn't a blacklisted trigger...!")
+
+        elif successful == len(to_unblacklist):
+            msg.reply_text(
+                "Removed <code>{}</code> triggers from the blacklist.".format(
+                    successful), parse_mode=ParseMode.HTML)
+
+        elif not successful:
+            msg.reply_text(
+                "None of these triggers exist, so they weren't removed.".format(
+                    successful, len(to_unblacklist) - successful), parse_mode=ParseMode.HTML)
+
+        else:
+            msg.reply_text(
+                "Removed <code>{}</code> triggers from the blacklist. {} did not exist, "
+                "so were not removed.".format(successful, len(to_unblacklist) - successful),
+                parse_mode=ParseMode.HTML)
+    else:
+        msg.reply_text("Tell me which words you would like to remove from the blacklist.")
+
+
+@run_async
+@user_not_admin
+def del_blacklist(bot: Bot, update: Update):
+    chat = update.effective_chat  # type: Optional[Chat]
+    message = update.effective_message  # type: Optional[Message]
+    to_match = extract_text(message)
+    if not to_match:
+        return
+
+    chat_filters = sql.get_chat_blacklist(chat.id)
+    for trigger in chat_filters:
+        pattern = r"( |^|[^\w])" + re.escape(trigger) + r"( |$|[^\w])"
+        if re.search(pattern, to_match, flags=re.IGNORECASE):
+            try:
+                message.delete()
+            except BadRequest as excp:
+                if excp.message == "Message to delete not found":
+                    pass
+                else:
+                    LOGGER.exception("Error while deleting blacklist message. KUCH TO GADBAD H DAYA")
+            break
+
+
+def __migrate__(old_chat_id, new_chat_id):
+    sql.migrate_chat(old_chat_id, new_chat_id)
+
+
+def __chat_settings__(chat_id, user_id):
+    blacklisted = sql.num_blacklist_chat_filters(chat_id)
+    return "There are {} blacklisted words.".format(blacklisted)
+
+
+def __stats__():
+    return "{} blacklist triggers, across {} chats.".format(sql.num_blacklist_filters(),
+                                                            sql.num_blacklist_filter_chats())
+
+
+__mod_name__ = "Word Blacklists"
+
+__help__ = """
+Blacklist ke andar jo words honge,un words ko group me use karte hi wo delete ho jaenge , \
+Admins ke words delete nhi honge!
+
+......
+
+ - /blacklist: current blacklisted words ki list
+
+*Admin only:*
+ - /addblacklist <triggers>: Add a word to the blacklist. har ek line pe ek trigger \
+     isse ek sath 1+ triggers add kar sakte h
+ - /unblacklist <triggers>: Remove a word from the blacklist. Same newline logic applies here, so you can remove \
+multiple triggers at once.
+ - /rmblacklist <triggers>: Same as above.
+"""
+
+BLACKLIST_HANDLER = DisableAbleCommandHandler("blacklist", blacklist, filters=Filters.group, pass_args=True,
+                                              admin_ok=True)
+ADD_BLACKLIST_HANDLER = CommandHandler("addblacklist", add_blacklist, filters=Filters.group)
+UNBLACKLIST_HANDLER = CommandHandler(["unblacklist", "rmblacklist"], unblacklist, filters=Filters.group)
+BLACKLIST_DEL_HANDLER = MessageHandler(
+    (Filters.text | Filters.command | Filters.sticker | Filters.photo) & Filters.group, del_blacklist)
+
+dispatcher.add_handler(BLACKLIST_HANDLER)
+dispatcher.add_handler(ADD_BLACKLIST_HANDLER)
+dispatcher.add_handler(UNBLACKLIST_HANDLER)
+dispatcher.add_handler(BLACKLIST_DEL_HANDLER, group=BLACKLIST_GROUP)
---
+++ tg_bot/modules/muting.py
@@ -0,0 +1,195 @@
+import html
+from typing import Optional, List
+
+from telegram import Message, Chat, Update, Bot, User
+from telegram.error import BadRequest
+from telegram.ext import CommandHandler, Filters
+from telegram.ext.dispatcher import run_async
+from telegram.utils.helpers import mention_html
+
+from tg_bot import dispatcher, LOGGER
+from tg_bot.modules.helper_funcs.chat_status import bot_admin, user_admin, is_user_admin, can_restrict
+from tg_bot.modules.helper_funcs.extraction import extract_user, extract_user_and_text
+from tg_bot.modules.helper_funcs.string_handling import extract_time
+from tg_bot.modules.log_channel import loggable
+
+
+@run_async
+@bot_admin
+@user_admin
+@loggable
+def mute(bot: Bot, update: Update, args: List[str]) -> str:
+    chat = update.effective_chat  # type: Optional[Chat]
+    user = update.effective_user  # type: Optional[User]
+    message = update.effective_message  # type: Optional[Message]
+
+    user_id = extract_user(message, args)
+    if not user_id:
+        message.reply_text("Mute karne ke liye kisi ko mention kare ya unke msg ka reply dete hue command kare.")
+        return ""
+
+    if user_id == bot.id:
+        message.reply_text("Manu ( @king_un_007 ) ko bolunga ki muje mute karn chahte ho!")
+        return ""
+
+    member = chat.get_member(int(user_id))
+
+    if member:
+        if is_user_admin(chat, user_id, member=member):
+            message.reply_text("Admin owner ko boldega ki maine use mute kiya!")
+
+        elif member.can_send_messages is None or member.can_send_messages:
+            bot.restrict_chat_member(chat.id, user_id, can_send_messages=False)
+            message.reply_text("👍🏻 chuppp! 🤐")
+            return "<b>{}:</b>" \
+                   "\n#MUTE" \
+                   "\n<b>Admin:</b> {}" \
+                   "\n<b>User:</b> {}".format(html.escape(chat.title),
+                                              mention_html(user.id, user.first_name),
+                                              mention_html(member.user.id, member.user.first_name))
+
+        else:
+            message.reply_text("Ye user pehle se hi mute h!")
+    else:
+        message.reply_text("ye user group me nhi h")
+
+    return ""
+
+
+@run_async
+@bot_admin
+@user_admin
+@loggable
+def unmute(bot: Bot, update: Update, args: List[str]) -> str:
+    chat = update.effective_chat  # type: Optional[Chat]
+    user = update.effective_user  # type: Optional[User]
+    message = update.effective_message  # type: Optional[Message]
+
+    user_id = extract_user(message, args)
+    if not user_id:
+        message.reply_text("unMute karne ke liye kisi ko mention kare ya unke msg ka reply dete hue command kare.")
+        return ""
+
+    member = chat.get_member(int(user_id))
+
+    if member.status != 'kicked' and member.status != 'left':
+        if member.can_send_messages and member.can_send_media_messages \
+                and member.can_send_other_messages and member.can_add_web_page_previews:
+            message.reply_text("ye user pehle se hi Unmuted h.")
+        else:
+            bot.restrict_chat_member(chat.id, int(user_id),
+                                     can_send_messages=True,
+                                     can_send_media_messages=True,
+                                     can_send_other_messages=True,
+                                     can_add_web_page_previews=True)
+            message.reply_text("Ab bol sakta h!")
+            return "<b>{}:</b>" \
+                   "\n#UNMUTE" \
+                   "\n<b>Admin:</b> {}" \
+                   "\n<b>User:</b> {}".format(html.escape(chat.title),
+                                              mention_html(user.id, user.first_name),
+                                              mention_html(member.user.id, member.user.first_name))
+    else:
+        message.reply_text("ye user group me hi nhui h inko unmute karne se apne ap group me nhi ane "
+                           "wale h wo!")
+
+    return ""
+
+
+@run_async
+@bot_admin
+@can_restrict
+@user_admin
+@loggable
+def temp_mute(bot: Bot, update: Update, args: List[str]) -> str:
+    chat = update.effective_chat  # type: Optional[Chat]
+    user = update.effective_user  # type: Optional[User]
+    message = update.effective_message  # type: Optional[Message]
+
+    user_id, reason = extract_user_and_text(message, args)
+
+    if not user_id:
+        message.reply_text("Kisi ko tag karo ya mention karke command use karo.")
+        return ""
+
+    try:
+        member = chat.get_member(user_id)
+    except BadRequest as excp:
+        if excp.message == "User not found":
+            message.reply_text("wo user group me hi nhui h ")
+            return ""
+        else:
+            raise
+
+    if is_user_admin(chat, user_id, member):
+        message.reply_text("Admins ko mute kon nhi karna chahega...")
+        return ""
+
+    if user_id == bot.id:
+        message.reply_text("Pagal nhi hu jo khudko mute karunga!")
+        return ""
+
+    if not reason:
+        message.reply_text("kitni der ke liye mute karna h?!")
+        return ""
+
+    split_reason = reason.split(None, 1)
+
+    time_val = split_reason[0].lower()
+    if len(split_reason) > 1:
+        reason = split_reason[1]
+    else:
+        reason = ""
+
+    mutetime = extract_time(message, time_val)
+
+    if not mutetime:
+        return ""
+
+    log = "<b>{}:</b>" \
+          "\n#TEMP MUTED" \
+          "\n<b>Admin:</b> {}" \
+          "\n<b>User:</b> {}" \
+          "\n<b>Time:</b> {}".format(html.escape(chat.title), mention_html(user.id, user.first_name),
+                                     mention_html(member.user.id, member.user.first_name), time_val)
+    if reason:
+        log += "\n<b>Reason:</b> {}".format(reason)
+
+    try:
+        if member.can_send_messages is None or member.can_send_messages:
+            bot.restrict_chat_member(chat.id, user_id, until_date=mutetime, can_send_messages=False)
+            message.reply_text("Chupp! 😠 Muted for {}!".format(time_val))
+            return log
+        else:
+            message.reply_text("user pehle se hi muted.")
+
+    except BadRequest as excp:
+        if excp.message == "Reply message not found":
+            # Do not reply
+            message.reply_text("Chupp! 😠 Muted for {}!".format(time_val), quote=False)
+            return log
+        else:
+            LOGGER.warning(update)
+            LOGGER.exception("ERROR muting user %s in chat %s (%s) due to %s", user_id, chat.title, chat.id,
+                             excp.message)
+            message.reply_text("Well damn, kuch to gadbad h daya.")
+
+    return ""
+
+
+__help__ = """
+*Admin only:*
+ - /mute <userhandle>: silences a user. Can also be used as a reply, muting the replied to user.
+ - /tmute <userhandle> x(m/h/d): mutes a user for x time. (via handle, or reply). m = minutes, h = hours, d = days.
+ - /unmute <userhandle>: unmutes a user. Can also be used as a reply, muting the replied to user.
+"""
+
+__mod_name__ = "Mute"
+
+MUTE_HANDLER = CommandHandler("mute", mute, pass_args=True, filters=Filters.group)
+UNMUTE_HANDLER = CommandHandler("unmute", unmute, pass_args=True, filters=Filters.group)
+TEMPMUTE_HANDLER = CommandHandler(["tmute", "tempmute"], temp_mute, pass_args=True, filters=Filters.group)
+
+dispatcher.add_handler(MUTE_HANDLER)
+dispatcher.add_handler(UNMUTE_HANDLER)
+dispatcher.add_handler(TEMPMUTE_HANDLER)
